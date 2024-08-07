dbcd58ca7c830632926e9b28dddcc0017b7eaa40
Xoaquin Sanchez Varsallona <xsanchezvarsallona@mail.austral.edu.ar>
2024-08-07 13:57:12 -0300

    .

--- src/main/java/printScreen/parser/genTree/genTree.kt
+++ src/main/java/printScreen/parser/genTree/genTree.kt
@@ -1,11 +1,20 @@
 package printScreen.parser.genTree
 
 import printScreen.models.ast.AST
+import printScreen.models.ast.Node
 import token.Token
 
 class genTree {
 
-    fun tockenToAST (token: Token)  : AST {
+    fun tockenToAST ( tokens : List<Token>? )  : List<AST> {
+        //TODO: Aca se pasa de Token a AST
+    }
+
+    fun rootTocken (token : Token) : AST {
+        //TODO: Caso donde () o * o / para que se maneje bien la prioridad de las operaciones.
+    }
+
+    fun createTree (token: Token) : Node {
 
     }
 }
--- src/main/java/printScreen/parser/verify/verifyAST.kt
+++ src/main/java/printScreen/parser/verify/verifyAST.kt
@@ -3,7 +3,10 @@ import printScreen.models.ast.AST
 
 class verifyAST {
 
-    fun verify (tree : AST) : Boolean {
+    // Esto lo que tiene que hacer es verificar que el AST
+    // que se pase tenga bien el typado y que las cosas tengan sentido.
+
+    fun verify (list : List<AST>) : Boolean {
         return false
     }
 }
