dcc1dd373817c319e296690afd67d3d54d8dec02
Marcos <mcarrizoaslanian@mail.austral.edu.ar>
2024-08-08 16:29:07 -0300

    new lexer (mejor)

--- src/main/java/printScreen/lexer/Lexer.kt
+++ src/main/java/printScreen/lexer/Lexer.kt
@@ -1,109 +1,59 @@
 package printScreen.lexer
 
-import printScreen.models.token.NonValuedToken
 import printScreen.models.token.Token
 import printScreen.models.token.TokenType
 import printScreen.models.token.ValuedToken
 
-class Lexer {
-    fun lex(input: String): List<Token>? {
-        val result: MutableList<Token> = mutableListOf()
-        var column = 1
-        var line = 1
-        var i = 0
-        while (i < input.length) {
-            if (isWhitespace(input[i])) {
-                if (input[i] == '\n') {
-                    line++
-                    column = 1
-                } else {
-                    column++
-                }
-            } else if (isOperator(input[i])) {
-                result.add(ValuedToken(TokenType.OPERATOR, input[i].toString(), line, column))
-                column++
-            } else if (isSyntax(input[i])) {
-                result.add(ValuedToken(TokenType.SYNTAX, input[i].toString(), line, column))
-                column++
-            } else if (isLiteral(input[i])) {
-                if (input[i] == '"') {
-                    val end = checkString(input, i)
-                    result.add(ValuedToken(TokenType.LITERAL, input.substring(i, end), line, column))
-                    column += end - i + 1
-                    i = end
-                } else {
-                    val end = checkNumber(input, i)
-                    result.add(ValuedToken(TokenType.LITERAL, input.substring(i, end + 1), line, column))
-                    column += end - i + 1
-                    i = end
-                }
-            } else {
-                val end = checkForToken(input.substring(i), line, column)
-                if (end != null) {
-                    result.add(ValuedToken(TokenType.KEYWORD, input.substring(i, i + end), line, column))
-                    column += end
-                    i += end
-                } else {
-                    throw Exception("Invalid character at line $line, column $column")
-                }
-            }
-            i++
+class Lexer(private val sourceCode: String) {
+    private var position: Int = 0
+    private var line: Int = 1
+    private var column: Int = 1
+
+    private val tokenPatterns = listOf(
+        "\\blet\\b" to TokenType.KEYWORD,
+        "\\bstring\\b" to TokenType.KEYWORD,
+        "\\bnumber\\b" to TokenType.KEYWORD,
+        "\\bprintln\\b" to TokenType.KEYWORD,
+        "[a-zA-Z_][a-zA-Z_0-9]*" to TokenType.IDENTIFIER,
+        "[0-9]+(\\.[0-9]+)?" to TokenType.LITERAL,
+        "\"[^\"]*\"|'[^']*'" to TokenType.LITERAL,
+        "[+\\-*/]" to TokenType.OPERATOR,
+        "[=:;()]" to TokenType.SYNTAX
+    )
+
+    fun tokenize(): List<Token> {
+        val tokens = mutableListOf<Token>()
+        while (position < sourceCode.length) {
+            val (token, newPos) = getNextToken()
+            if (token != null) tokens.add(token)
+            position = newPos
         }
+        return tokens
     }
 
-    fun isWhitespace(char: Char): Boolean {
-        return char == ' ' || char == '\t' || char == '\n'
-    }
+    private fun getNextToken(): Pair<Token?, Int> {
 
-    fun isOperator(char: Char): Boolean {
-        return char == '+' || char == '-' || char == '*' || char == '/' || char == '%' || char == '=' || char == '!' || char == '>' || char == '<' || char == '&' || char == '|'
-    }
-
-    fun isSyntax(char: Char): Boolean {
-        return char == '(' || char == ')' || char == '{' || char == '}' || char == '[' || char == ']' || char == ',' || char == ';' || char == '.'
-    }
-
-    fun isLiteral(char: Char): Boolean {
-        return char == '"' || char == '\'' || char.isDigit()
-    }
-
-    fun checkString(input: String, index: Int): Int { // index is the index of the first character of the string
-        var i = index + 1
-        while (i < input.length) {
-            if (input[i] == '"') {
-                return i + 1
+        while (position < sourceCode.length && sourceCode[position].isWhitespace()) {
+            if (sourceCode[position] == '\n') {
+                line++
+                column = 1
             }
-            i++
+            column++
+            position++
         }
-        throw Exception("String not closed")
-    }
 
-    fun checkNumber(input: String, index: Int): Int {
-        var i = index + 1
-        while (i < input.length) {
-            if (!input[i].isDigit()) {
-                return i
-            }
-            i++
-        }
-        return i
-    }
+        if (position >= sourceCode.length) return null to position
 
-    fun checkKeyWord(input: String, index: Int): Int? {
-        val keywords = listOf("if", "else", "while", "for", "fun", "return", "break", "continue", "let", "true", "false")
-        var i = index
-        var word = input[i].toString()
-        while (i < input.length) {
-            if (word in keywords && ) {
-                return word.length
+        for ((pattern, tokenType) in tokenPatterns) {
+            val regex = Regex(pattern)
+            val matchResult = regex.find(sourceCode, position)
+            if (matchResult != null && matchResult.range.first == position) {
+                val value = matchResult.value
+                val token = ValuedToken(tokenType, value, line, column)
+                column += value.length
+                return token to position + value.length
             }
-            if (input[i].isLetterOrDigit() || input[i] == '_') {
-                word += input[i]
-            } else {
-                break
-            }
-            i++
         }
-        return null
+        throw IllegalArgumentException("Unexpected character at position $position")
     }
 }
