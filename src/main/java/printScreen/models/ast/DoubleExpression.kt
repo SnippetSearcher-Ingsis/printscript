eecefbe9f6ef969f3727083fb369c5ae58c22d35
Xoaquin Sanchez Varsallona <xsanchezvarsallona@mail.austral.edu.ar>
2024-08-08 16:37:41 -0300

    ast format

--- src/main/java/printScreen/models/ast/DoubleExpression.kt
+++ src/main/java/printScreen/models/ast/DoubleExpression.kt
@@ -2,6 +2,16 @@ package printScreen.models.ast
 
 class DoubleExpression (private val left: Node, private val operator: String, private val right: Node) : Node {
 
+    fun left () : Node {
+        return left
+    }
 
+    fun right () : Node {
+        return right
+    }
+
+    fun operator () : String {
+        return operator
+    }
 
 }
--- src/main/java/printScreen/models/ast/Literal.kt
+++ src/main/java/printScreen/models/ast/Literal.kt
@@ -1,10 +1,7 @@
 package printScreen.models.ast
 
-class Literal <X> (private val type : String, private val value: X) : Node {
-    fun value () : X {
+class Literal <X> (private val value: X?) : Node {
+    fun value () : X? {
         return value
-    }
-    fun type () : String {
-        return type
     }
 }
