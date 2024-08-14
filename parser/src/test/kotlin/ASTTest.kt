e55d294bc182dfb9fff0c23a4af0a08c46a543e1
Xoaquin Sanchez Varsallona <xsanchezvarsallona@mail.austral.edu.ar>
2024-08-14 18:00:46 -0300

    test de parentesis y correcion de bug

--- parser/src/test/kotlin/ASTTest.kt
+++ parser/src/test/kotlin/ASTTest.kt
@@ -49,4 +49,24 @@ class ASTTest {
         val result =  ASTGenerator().tokensToAST(example)
         println(result)
     }
+
+    @Test
+    fun parenthesisTest () {
+        val example = listOf(
+            ValuedToken(TokenType.KEYWORD, "let", 1,1 ),
+            ValuedToken(TokenType.IDENTIFIER, "a", 1,5 ),
+            ValuedToken(TokenType.SYNTAX, ":", 1,7 ),
+            ValuedToken(TokenType.TYPE, "number", 1,9 ),
+            ValuedToken(TokenType.EQUAL, "=", 1,16 ),
+            ValuedToken(TokenType.SYNTAX, "(", 1,16 ),
+            ValuedToken(TokenType.LITERAL, "9", 1,16 ),
+            ValuedToken(TokenType.SYNTAX, "+", 1,16 ),
+            ValuedToken(TokenType.LITERAL, "8", 1,16 ),
+            ValuedToken(TokenType.SYNTAX, ")", 1,16 ),
+            ValuedToken(TokenType.SEMICOLON, ";", 1,31 ),
+        )
+
+        val result =  ASTGenerator().tokensToAST(example)
+        println(result)
+    }
 }
