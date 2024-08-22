d3335e837e56f2205fde490122e2c4209962be00
Marcos <mcarrizoaslanian@mail.austral.edu.ar>
2024-08-22 16:09:03 -0300

    fix XOACO

---
+++ cli/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "2.0.0"// Especifica la versión del plugin de Kotlin
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.11.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8") // Cambiado a la dependencia completa de Kotlin
+    implementation(project(":models"))
+    implementation(project(":formatter"))
+    implementation(project(":lexer"))
+    implementation(project(":linter"))
+    implementation(project(":parser"))
+    implementation(project(":interpreter"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain {
+        (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(19)) // Especifica la versión de Java
+    }
+}
