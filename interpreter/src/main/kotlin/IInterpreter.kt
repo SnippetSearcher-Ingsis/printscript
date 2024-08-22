bf807d38e23643d00545c23e39f5f285ee1ff240
pedro <pedroramirezneira@gmail.com>
2024-08-22 16:14:36 -0300

    Commit solo para sumarme al comentario de mi compañero, con esta gente no se puede

--- interpreter/src/main/kotlin/IInterpreter.kt
+++ interpreter/src/main/kotlin/IInterpreter.kt
@@ -2,6 +2,7 @@ import node.ASTNode
 
 /**
  * Interface for the interpreter.
+ * No se ni que escribo, es solo para commitear.
  */
 interface IInterpreter {
   infix fun interpret(nodes: List<ASTNode>)
