f45a1939815598b00edc5c5ce430f67399da00bf
pedro <pedroramirezneira@gmail.com>
2024-08-29 11:42:07 -0300

    Addition of option to toggle the interpreter's print to the standard output

--- interpreter/src/main/kotlin/TracingInterpreter.kt
+++ interpreter/src/main/kotlin/TracingInterpreter.kt
@@ -7,13 +7,14 @@ import visitor.TracingVisitor
 
 /**
  * Interpreter that logs the execution of the program.
+ * @param print If true, the interpreter will print the execution of the program to the standard output.
  */
-class TracingInterpreter : IInterpreter, ILog {
+class TracingInterpreter(private val print: Boolean = true) : IInterpreter, ILog {
   private val logger = Logger()
 
   private val context = Context()
 
-  private val visitor = TracingVisitor(context, EvaluationVisitor(context), logger)
+  private val visitor = TracingVisitor(context, EvaluationVisitor(context), logger, print)
 
   override fun interpret(nodes: List<ASTNode>) {
     nodes.forEach { it.accept(visitor) }
