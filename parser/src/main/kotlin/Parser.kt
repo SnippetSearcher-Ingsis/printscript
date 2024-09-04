01e666ad7facc0e722b9e780047c5dd90a621d5b
pedro <pedroramirezneira@gmail.com>
2024-09-04 12:27:32 -0300

    Update version 1.1

--- parser/src/main/kotlin/Parser.kt
+++ parser/src/main/kotlin/Parser.kt
@@ -8,6 +8,7 @@ import token.TokenType
 class Parser : IParser {
 
   override fun parse(tokens: Iterator<List<Token>>): Iterator<ASTNode> {
+    println("Hello, World!")
     return ParserIterator(tokens)
   }
 
