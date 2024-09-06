034a324d8cc9230faf363cf4a3a88b5e8f663f21
Pedro Ramirez Neira <65603558+pedroramirezneira@users.noreply.github.com>
2024-09-06 19:42:26 -0300

    Fix merge (#61)

--- parser/src/main/kotlin/com/printscript/parser/builder/AssignationBuilder.kt
+++ parser/src/main/kotlin/com/printscript/parser/builder/AssignationBuilder.kt
@@ -1,14 +1,21 @@
 package com.printscript.parser.builder
 
-class AssignationBuilder(private val line: List<com.printscript.models.token.Token>) : Builder {
-  override fun build(): com.printscript.models.node.ASTNode {
-    val handler = com.printscript.models.token.TokenHandler(line)
-    val identifier = handler.consume(com.printscript.models.token.TokenType.IDENTIFIER, "Se esperaba el nombre de la variable.")
+import com.printscript.models.node.ASTNode
+import com.printscript.models.node.AssignationNode
+import com.printscript.models.node.Position
+import com.printscript.models.token.Token
+import com.printscript.models.token.TokenHandler
+import com.printscript.models.token.TokenType
+
+class AssignationBuilder(private val line: List<Token>) : Builder {
+  override fun build(): ASTNode {
+    val handler = TokenHandler(line)
+    val identifier = handler.consume(TokenType.IDENTIFIER, "Se esperaba el nombre de la variable.")
     val name = identifier.value
-    val position = com.printscript.models.node.Position(identifier.line, identifier.column)
-    handler.consume(com.printscript.models.token.TokenType.EQUAL, "Se esperaba '=' después del nombre de la variable.")
+    val position = Position(identifier.line, identifier.column)
+    handler.consume(TokenType.EQUAL, "Se esperaba '=' después del nombre de la variable.")
     val expressionTokens = handler.collectExpressionTokens(false)
-    handler.consume(com.printscript.models.token.TokenType.SEMICOLON, "Se esperaba ';' después de la declaración.")
-    return com.printscript.models.node.AssignationNode(name, ExpressionBuilder(expressionTokens).build(), position)
+    handler.consume(TokenType.SEMICOLON, "Se esperaba ';' después de la declaración.")
+    return AssignationNode(name, ExpressionBuilder(expressionTokens).build(), position)
   }
 }
