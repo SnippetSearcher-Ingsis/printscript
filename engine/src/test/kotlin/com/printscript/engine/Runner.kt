c771ca5459a047646959200446bcce26e3a14431
Marcos <mcarrizoaslanian@mail.austral.edu.ar>
2024-09-11 21:18:32 -0300

    improved cli

--- engine/src/test/kotlin/com/printscript/engine/Runner.kt
+++ engine/src/test/kotlin/com/printscript/engine/Runner.kt
@@ -1,6 +1,6 @@
 package com.printscript.engine
 
-import com.printscript.interpreter.PrintInterpreter
+import com.printscript.formatter.Formatter
 import com.printscript.lexer.Lexer
 import com.printscript.parser.PrintParser
 import java.io.File
@@ -9,5 +9,8 @@ fun main() {
   val lexer = Lexer()
   val parser = PrintParser()
   val test = parser.parse(lexer.lex(File("engine/src/test/resources/test.txt").reader()))
-  PrintInterpreter().interpret(test)
+  while (test.hasNext()) {
+    val node = test.next()
+    println(Formatter.format(node, File("engine/src/test/resources/formatterConfig.json")))
+  }
 }
