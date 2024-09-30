adb3a43cb44a178d8ac2d4e40dbd8e4ba75f4b14
Pedro Ramirez Neira <65603558+pedroramirezneira@users.noreply.github.com>
2024-09-30 01:28:12 -0300

    Fixed Dependabot Alert (#95)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     kotlin("jvm") version "2.0.0"
 }
 
+dependencies {
+    implementation("org.eclipse.jgit:org.eclipse.jgit:5.13.3.202401111512-r")
+}
+
 allprojects {
     group = "com.printscript"
     version = "1.1-SNAPSHOT"
